ccc5e21662e569558bf2d3c9468e8e6205df533f
Levi Aviv <leviaviv28@gmail.com>
2020-02-08 12:47:54 -0500

    Reimplemnted server code using Solace messaging queues

--- Server/Server.py
+++ Server/Server.py
@@ -1,26 +1,39 @@
-import flask
-import json
-
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-
-packages = {}
-routers = {}
+import certifi
+import paho.mqtt.client as mqtt
 
 
-@app.route('/update_location', methods=['POST'])
-def update_location():
-    tracking = flask.request.form['tracking']
-    node = flask.request.form['node']
+def updateDatabase(msg):
+    return
 
-    packages[tracking] = routers[node]
-    
-    return "Received"
+def getDatabase(msg):
+    return None, None
 
-
-@app.route('/get_location/<int:tracking>', methods=['GET'])
-def get_location(tracking):   
-    return json.dumps(packages[tracking])
+# Callback on connection
+def on_connect(client, userdata, flags, rc):
+    print(rc)
+    client.subscribe('update_tracking')
+    client.subscribe('fetch_tracking')
 
 
-app.run()
+# Callback when message is received
+def on_message(client, userdata, msg):
+    print(f'Message received on topic: {msg.topic}. Message: {msg.payload}')
+    if msg.topic == 'update_tracking':
+        updateDatabase(msg.payload)
+    elif msg.topic  == 'fetch_tracking':
+        tracking_num, package_state = getDatabase(msg.payload)
+        client.publish(f'fetch_tracking/{tracking_num}', payload=package_state)
+
+
+if __name__ == "__main__":
+    client = mqtt.Client()
+
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.tls_set(ca_certs=certifi.where())
+    client.username_pw_set('solace-cloud-client', 'vvngpe3cn2ss72a7425hgf1l7f')
+
+    client.connect('mr2hd0llj3vwjx.messaging.solace.cloud', port=8883)
+
+    client.loop_forever()
