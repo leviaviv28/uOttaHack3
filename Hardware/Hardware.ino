2ce8be1758b45a7e7bd0196638b0a0e4cde12c66
Yassine1231 <mbouh050@uottawa.ca>
2020-02-08 12:07:02 -0500

    Fixed problems

--- Hardware/Hardware.ino
+++ Hardware/Hardware.ino
@@ -8,15 +8,12 @@ volatile int interruptCounter;
 int totalInterruptCounter;
 hw_timer_t * timer = NULL;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
-
-const char* wifiPrefix = "InnovaChamps"
-const char* serverAddress = "http://192.168.137.109:5000/update_location";
-//const char* ssid = "ArduinoNet";
+long tracking = 123456789;
+const char* wifiPrefix = "InnovaChamps";
+const char* serverAddress = "http://34.66.100.203:80/update_location";
+const char* ssid = "ArduinoNet";
 const char* password =  "nohackplz";
 
-bool isValidWiFi(ssid) {
-	return strlen(ssid) > strlen(wifiPrefix) && ssid.startsWith(wifiPrefix);
-}
 
 void scanWiFi() {
 	Serial.println("scan start");
@@ -27,12 +24,27 @@ void scanWiFi() {
 	} else {
 		Serial.print(n);
 		Serial.println(" networks found");
-		for (int i = 0; i < n; ++i) {
+		for (int i = 0; i < n; i++) {
 			// Print SSID and RSSI for each network found
-			Serial.println((i + 1) + ": " + WiFi.SSID(i) + " (" + WiFi.RSSI(i) + ") " + (WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+			// Print SSID and RSSI for each network found
+            Serial.print(i + 1);
+            Serial.print(": ");
+            Serial.print(WiFi.SSID(i));
+            Serial.print(" (");
+            Serial.print(WiFi.RSSI(i));
+            Serial.print(")");
+            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+            delay(10);
+            Serial.println(WiFi.RSSI(i));
+            Serial.println(WiFi.RSSI());
 			// Check if WiFi is valid InnovaPost address, and that signal is stronger than current connection
-			if wifi.isValidWiFi(WiFi.SSID(i) && WiFi.RSSI(i) < WiFi.RSSI()) {
-				WiFi.begin(WiFi.SSID(i), password);
+        char __SSID[sizeof(WiFi.SSID(i))];
+        WiFi.SSID(i).toCharArray(__SSID, sizeof(__SSID));
+        Serial.println(__SSID);
+			if (WiFi.RSSI() == 0 || WiFi.RSSI(i) > WiFi.RSSI) {
+				WiFi.begin(__SSID, password);
+       Serial.println(__SSID);
+       delay(3000);
 				while (WiFi.status() != WL_CONNECTED) //Check for the connection
 					Serial.println("Connecting to " + WiFi.SSID(i));
 				Serial.println("Connected to " + WiFi.SSID(i));
@@ -47,7 +59,7 @@ void sendUpdate(int retries) {
 	http.begin(serverAddress);  //Specify destination for HTTP request
 	http.addHeader("Content-Type", "text/plain");             //Specify content-type header
 
-	int httpResponseCode = http.POST("tracking=" + tracking + "&node=" + WiFi.SSID());   //Send the actual POST request
+	int httpResponseCode = http.POST("tracking=" + String(tracking) +"&node=" + WiFi.SSID());   //Send the actual POST request
 
 	if(httpResponseCode>0){
 		String response = http.getString(); //Get the response to the request
@@ -56,9 +68,12 @@ void sendUpdate(int retries) {
 	} else {
 		Serial.print("Error on sending POST: ");
 		Serial.println(httpResponseCode);
-		Serial.println("Retrying " + retries + " times")
+		Serial.print("Retrying ");
+      Serial.print(retries);
+    Serial.println(" times");
+
 		if(retries > 0)
-			sendUpdate(retries) - 1;
+			sendUpdate(retries - 1);
 	}
 
 	http.end();  //Free resources
@@ -66,14 +81,13 @@ void sendUpdate(int retries) {
 
 void setup() {
 	Serial.begin(115200);
-
+/*
 	// Timer ISR Setup
 	// Set timer to trigger every 10 seconds: 80k/800k=10s
 	timer = timerBegin(1, 800000000, true);
-	timerAttachInterrupt(timer, &onTimer, true);
+	timerAttachInterrupt(timer, onTimer, true);
 	timerAlarmWrite(timer, 1000000, true);
-	timerAlarmEnable(timer);
-
+	timerAlarmEnable(timer);*/
 	WiFi.mode(WIFI_STA);
 	WiFi.disconnect();
 	delay(100);
@@ -83,7 +97,7 @@ void setup() {
 	scanWiFi();
  }
 
- void IRAM_ATTR onTime() {
+/*void IRAM_ATTR onTime() {
 	portENTER_CRITICAL_ISR(&timerMux);
 	interrupts++;
 	if(WiFi.status() == WL_CONNECTED) {   //Check WiFi connection status
@@ -92,15 +106,23 @@ void setup() {
 		scanWiFi();
 	}
 	portEXIT_CRITICAL_ISR(&timerMux);
-}
+}*/
 
 void loop() {
-	if (interrupts > 0) {
+ 
+	/*if (interrupts > 0) {
 		portENTER_CRITICAL(&timerMux);
 		interrupts--;
 		portEXIT_CRITICAL(&timerMux);
 		totalInterrupts++;
 		Serial.print("totalInterrupts");
 		Serial.println(totalInterrupts);
-	}
+	}*/
+
+  if(WiFi.status() == WL_CONNECTED) {   //Check WiFi connection status
+    sendUpdate(MAX_RETRY);
+  } else {
+    scanWiFi();
+  }
+  delay(5000);
 }
