6e3640fbb78a81fa3944133fd347600c7129c027
Levi Aviv <leviaviv28@gmail.com>
2020-02-08 01:46:58 -0500

    Initial implementation of GET and POST handling

--- Server/Server.py
+++ Server/Server.py
@@ -1,11 +1,26 @@
 import flask
+import json
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+packages = {}
+routers = {}
+
 
 @app.route('/update_location', methods=['POST'])
-def update():
-    return "<p>Received</p>"
+def update_location():
+    tracking = flask.request.form['tracking']
+    node = flask.request.form['node']
+
+    packages[tracking] = routers[node]
+    
+    return "Received"
+
+
+@app.route('/get_location/<int:tracking>', methods=['GET'])
+def get_location(tracking):   
+    return json.dumps(packages[tracking])
+
 
 app.run()
