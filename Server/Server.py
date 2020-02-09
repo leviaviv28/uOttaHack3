2afbcdf7f0c1ed8e5d9f0a7607321eb855a58fc4
Yassine1231 <mbouh050@uottawa.ca>
2020-02-09 00:39:20 -0500

    Added environmental data message roots

--- Server/Server.py
+++ Server/Server.py
@@ -2,14 +2,19 @@ import certifi
 import paho.mqtt.client as mqtt
 
 packages = {}
+environment = {}
 
-def updateDatabase(msg):
+def updatePackage(msg):
     tracking = msg.route.split('/')[1]
     location = msg.payload
     packages[tracking] = location
 
+def updateEnvironment(msg):
+    tracking = msg.route.split('/')[1]
+    location = msg.payload
+    environment[tracking] = location
 
-def getDatabase(msg):
+def getPackage(msg):
     if msg.payload in packages.keys():
         return msg.payload, packages[msg.payload]
     else:
@@ -20,27 +25,24 @@ def on_connect(client, userdata, flags, rc):
     print(rc)
     client.subscribe('fetch_tracking/listen')
     client.subscribe('update_tracking/#')
-
+    client.subscribe('update_environment/#')
 
 # Callback when message is received
 def on_message(client, userdata, msg):
     print(f'Message received on topic: {msg.topic}. Message: {msg.payload}')
     if msg.topic == 'update_tracking':
-        updateDatabase(msg)
+        updatePackage(msg)
+    elif msg.topic == 'update_environment':
+        updateEnvironment(msg)
     elif msg.topic  == 'fetch_tracking':
-        tracking_num, package_state = getDatabase(msg.payload)
+        tracking_num, package_state = getPackage(msg.payload)
         client.publish(f'fetch_tracking/{tracking_num}', payload=package_state)
-
 
 if __name__ == "__main__":
     client = mqtt.Client()
-
     client.on_connect = on_connect
     client.on_message = on_message
-
     client.tls_set(ca_certs=certifi.where())
     client.username_pw_set('solace-cloud-client', 'vvngpe3cn2ss72a7425hgf1l7f')
-
     client.connect('mr2hd0llj3vwjx.messaging.solace.cloud', port=8883)
-
     client.loop_forever()
